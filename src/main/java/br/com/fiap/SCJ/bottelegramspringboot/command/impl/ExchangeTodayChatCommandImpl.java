2dc853c142db5426d5653212bb1da58b19e9c202
Felipe Marques <fmarques.ds@gmail.com>
2021-11-28 23:00:09 -0300

    removidos imports desnecessarios

--- src/main/java/br/com/fiap/SCJ/bottelegramspringboot/command/impl/ExchangeTodayChatCommandImpl.java
+++ src/main/java/br/com/fiap/SCJ/bottelegramspringboot/command/impl/ExchangeTodayChatCommandImpl.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.math.RoundingMode;
-import java.text.DecimalFormat;
 
 @Service
 @RequiredArgsConstructor
