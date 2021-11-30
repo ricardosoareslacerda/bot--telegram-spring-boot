9bf9215bf1e10347915eb7870b055488f65887c5
Ricardo Luiz Soares Lacerda <RLLacerda@rd.com.br>
2021-11-30 03:31:27 -0300

    Ajustes /clima e /previsao e ajustes nos parametros da cidade informada

---
+++ src/main/java/br/com/fiap/SCJ/bottelegramspringboot/command/impl/ForecastChatCommandImpl.java
@@ -0,0 +1,25 @@
+package br.com.fiap.SCJ.bottelegramspringboot.command.impl;
+
+import br.com.fiap.SCJ.bottelegramspringboot.command.ChatCommand;
+import br.com.fiap.SCJ.bottelegramspringboot.service.WeatherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Service
+@RequiredArgsConstructor
+public class ForecastChatCommandImpl implements ChatCommand {
+
+    private final WeatherService weatherAPIService;
+    private static final String COMANDO = "previsao";
+
+    @Override
+    public String comando() {
+        return COMANDO;
+    }
+
+    @Override
+    public String execute(Update update, final String city) {
+        return weatherAPIService.forecast(city);
+    }
+}
